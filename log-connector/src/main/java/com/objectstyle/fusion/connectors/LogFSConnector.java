1853f6ba08c19b6a036989deca42e7e6f95fc51a
Dzmitry Kazimirchyk <dkazimirchyk@gmail.com>
2015-12-31 14:21:08 +0300

    Remove redundant registration of log file type since we already register it in connectors.json config.

--- log-connector/src/main/java/com/objectstyle/fusion/connectors/LogFSConnector.java
+++ log-connector/src/main/java/com/objectstyle/fusion/connectors/LogFSConnector.java
@@ -5,11 +5,4 @@ import com.lucidworks.connectors.Connector;
 
 public class LogFSConnector extends Connector {
 
-    public static final String LOG_FILE_TYPE = "logFile";
-
-    @Override
-    public void init() {
-        super.init();
-        registerDataSourceType(LOG_FILE_TYPE, new LogFileType());
-    }
 }
